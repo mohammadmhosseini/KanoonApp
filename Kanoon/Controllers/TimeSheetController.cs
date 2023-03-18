b64dde568f407f97abbbc6a1896076344c6f078d
mohammadmhosseini <moh79hosseini@gmail.com>
2023-03-18 17:28:12 +0330

    Add project files.

---
+++ Kanoon/Controllers/TimeSheetController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class TimeSheetController : Controller
+    {
+
+        public ActionResult timeSheet()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult main()
+        {
+            return View();
+        }
+
+        public ActionResult report()
+        {
+            return View();
+        }
+
+        public ActionResult adminReport()
+        {
+            return View();
+        }
+
+        public ActionResult userWorkList()
+        {
+            return View();
+        }
+
+
+        public ActionResult insertUsersPayroll()
+        {
+            return View();
+        }
+
+        public ActionResult projectList()
+        {
+            return View();
+        }
+        public ActionResult definedWorkList()
+        {
+            return View();
+        }
+
+        public ActionResult usersVerifyWorksList()
+        {
+            return View();
+        }
+
+
+    }
+
+}
---
+++ Kanoon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kanoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
